71f1098a02ac44a4ed5442b31c5c098d5e15850b
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-04-22 21:55:47 +0300

    added a new screen to view the data that saved in the db from the api

---
+++ lib/screens/events_apil_local_db.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EventsApiLocalDb extends StatefulWidget {
+  @override
+  _EventsApiLocalDbState createState() => _EventsApiLocalDbState();
+}
+
+class _EventsApiLocalDbState extends State<EventsApiLocalDb> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
